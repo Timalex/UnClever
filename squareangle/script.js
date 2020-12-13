ce7330efa707aacd0f25cb386c3bcaae48c494a8
Timalex <tim.alexander.gunnarsson@gmail.com>
2020-12-13 01:36:28 +0100

    Add intro & outro. Accessible by contextual intro.

--- squareangle/script.js
+++ squareangle/script.js
@@ -81,9 +81,19 @@ addEventListener('pointerdown', (event) => {
 // Add steps to sequence
 sequencer.addStep('#triangleMain')
 sequencer.addStep('#squareC')
-sequencer.addStep('#squareB')
+sequencer.addStep('#squareC', { transform: ['translate(0, 0) rotate(0deg)', 'translate(-50px, -42px) rotate(21.8deg)'] })
 sequencer.addStep('#squareA')
-sequencer.addStep('#triangleOutline', { strokeWidth: [0, 0.5, 0], easing: ['ease-out', 'ease-out'] })
+sequencer.addStep('#squareA', { transform: ['translate(0, 0)', 'translate(50px, -58px)'] })
+sequencer.addStep('#squareB')
+sequencer.addStep('#squareB', { transform: ['translate(0, 0)', 'translate(65px, -73px)'] })
+sequencer.addStep('#questionedEqualTo')
+sequencer.addStep('#questionedEqualTo', [ { visibility: 'visible', opacity: 1, easing: 'ease-in'}, { visibility: 'hidden', opacity: 0} ])
+// sequencer.addStep('#observerEyeSymbol')
+// sequencer.addStep('#observerEyeSymbol', [ { visibility: 'visible', opacity: 1, easing: 'ease-in'}, { visibility: 'hidden', opacity: 0} ])
+sequencer.addStep('#squareC', { transform: ['translate(-50px, -42px) rotate(21.8deg)', 'translate(0, 0) rotate(0)'] })
+sequencer.addStep('#squareA', { transform: ['translate(50px, -58px)', 'translate(0, 0)'] })
+sequencer.addStep('#squareB', { transform: ['translate(65px, -73px)', 'translate(0, 0)'] })
+// sequencer.addStep('#triangleOutline', { strokeWidth: [0, 0.5, 0], easing: ['ease-out', 'ease-out'] })
 sequencer.addStep('#gridA')
 sequencer.addStep('#gridB')
 sequencer.addStep('#gridC')
@@ -91,5 +101,8 @@ sequencer.addStep('#slices', { shapeRendering: ['optimizeSpeed', 'geometricpreci
 sequencer.addStep('#smallA', { transform: ['translate(0, 0)', 'translate(20px, 50px)', 'translate(-30px, 70px)'] })
 sequencer.addStep('#bigB', { transform: ['translate(0, 0)', 'translate(8px,  20px)', 'translate(-50px, 20px)'] })
 sequencer.addStep('#bigA', { transform: ['translate(0, 0)', 'translate(20px, 50px)'] })
-
+sequencer.addStep('#gridPattern', [
+    { visibility: 'visible', opacity: 0.95, easing: 'ease-out' },
+    { visibility: 'hidden', opacity: 0 }],
+    { duration: 5000, fill: 'forwards'})
 sequencer.startSequence()
